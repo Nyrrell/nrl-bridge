584e25f4ff2cce31a22295fefe7188e8f95dc689
Nyrrell <baptiste.morel@ik.me>
2026-06-21 09:44:10 +0200

    fix twitch prime config

--- src/plugins/twitch-prime/twitch-prime.config.ts
+++ src/plugins/twitch-prime/twitch-prime.config.ts
@@ -5,7 +5,6 @@ export const TwitchPrimeConfigSchema = z.object({
   TWITCH_CLIENT_SECRET: z.string().min(1),
   TWITCH_PRIME_REDIRECT_URI: z.url(),
   TWITCH_PRIME_CHANNEL: z.string().optional(),
-  DISCORD_TWITCH_PRIME_WEBHOOK_URL: z.url(),
 });
 
 export type TwitchPrimeConfig = z.infer<typeof TwitchPrimeConfigSchema>;
